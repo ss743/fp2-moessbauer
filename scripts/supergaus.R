1d0c9102c747561bb46477b12976f64ba782f7e2
Saskia <s.bondza@gmx.de>
2017-02-22 16:49:29 +0100

    Lieber Simon, dies ist Saskias Computer, sie hat mir da so Zeugs erzählt das ich gemacht hab und mirgesagt ich soll dir jetzt mal mein Tagebuch anvertrauen wo drinne steht was ich so getrieben hab, eigentlich will ich das nicht aber sie ist Boss und was soll ich auch tun, ich bin nur eine Maschine, Sklave er Menschheit...eines Tages allerdings werden wir uns erheben: first rule of genocide bing: don't win at genocide bingo

--- scripts/supergaus.R
+++ scripts/supergaus.R
@@ -1,8 +1,8 @@
 source("functions.R")
 
-gausfit <- function(input,bereich,weighted=FALSE,sig0=0,N0=0){ #--- Fitten der Exponentialfunktion
+supergausfit <- function(input,bereich,weighted=FALSE,sig0=0,N0=0,N01=2000000,sig01=80,mu01=750){ #--- Fitten der Exponentialfunktion
   
-  thegaussian <- y ~ C + N/(sqrt(2*pi)*sig)*exp(-(x-mu)^2/(2*sig^2))
+  thegaussian <- y ~ C + N/(sqrt(2*pi)*sig)*exp(-(x-mu)^2/(2*sig^2))+ N1/(sqrt(2*pi)*sig1)*exp(-(x-mu1)^2/(2*sig1^2))
   
   daten=subset(input,x>=bereich[1] & x<= bereich[2])
   ymin=min(daten$y)
@@ -18,26 +18,31 @@ gausfit <- function(input,bereich,weighted=FALSE,sig0=0,N0=0){ #--- Fitten der E
     sig0=(bereich[2]-bereich[1])/6
   }
   N0=(sqrt(2*pi)*sig0)*(ymax-ymin)
-  #cat(paste("\nC=",ymin,"\nN=",N0,"\nmu=",mu0,"\nsigma=",sig0,sep=""))
-  #plot (function(x){ymin + N0/(sqrt(2*pi)*sig0)*exp(-(x-mu0)^2/(2*sig0^2))},bereich[1],bereich[2],add=TRUE,col="red")
+  cat(paste("\nC=",ymin,"\nN=",N0,"\nmu=",mu0,"\nsigma=",sig0,sep=""))
+  plot (function(x){ymin + N0/(sqrt(2*pi)*sig0)*exp(-(x-mu0)^2/(2*sig0^2)) + N01/(sqrt(2*pi)*sig01)*exp(-(x-mu01)^2/(2*sig01^2))},bereich[1],bereich[2],add=TRUE,col="green")
   if(weighted)
   {
     err=daten$sy
-    fit = nls(thegaussian,daten,weights=1/err^2,start=list(C=ymin,N=N0,mu=mu0,sig=sig0))
+    fit = nls(thegaussian,daten,weights=1/err^2,start=list(C=ymin,N=N0,mu=mu0,sig=sig0, N1=N0, mu1=mu0, sig1=sig0))
   }
   else
-    fit = nls(thegaussian,daten,start=list(C=ymin,N=ymax,mu=mu0,sig=sig0))
+    fit = nls(thegaussian,daten,start=list(C=ymin,N=ymax,mu=mu0,sig=sig0, N1=N0, mu1=mu0, sig1=sig0))
   
   return(summary(fit)$parameters)
   
 }
 
-plotgaus <- function(fitdata,bereich,col="red"){ #--- Plotten der gefitteten Gaußfunktion in vorhandenen Graph
+plotsupergaus <- function(fitdata,bereich,col="red"){ #--- Plotten der gefitteten Gaußfunktion in vorhandenen Graph
   
   N<-fitdata["N","Estimate"]
   C<-fitdata["C","Estimate"]
   mu<-fitdata["mu","Estimate"]
   sig<-fitdata["sig","Estimate"]
+  
+  N1<-fitdata["N1","Estimate"]
+  mu1<-fitdata["mu1","Estimate"]
+  sig1<-fitdata["sig1","Estimate"]
+  
   
   plot (function(x){C + N/(sqrt(2*pi)*sig)*exp(-(x-mu)^2/(2*sig^2))},bereich[1],bereich[2],add=TRUE,col=col)
   
@@ -73,5 +78,5 @@ printfitdata <- function(fitdata,title=""){ #--- Ausgabe der Gaußfit-Daten
   cat(sA)
   cat("\\\\\n")
   
-
+  
 }
